c17b4aeef6cf5c7a92e16fe51c23c8a10e1e1f8e
BraveWangDev <BraveWangDev@126.com>
2019-10-21 09:17:57 +0800

    修改缩进示例

--- _includes/javascripts/indentation.js
+++ _includes/javascripts/indentation.js
@@ -1,8 +1,12 @@
-var x = 1,
-    y = 1;
+// var x = 1,
+//     y = 1;
 
-if (x < y) {
-    x += 10;
-} else {
-    x += 1;
+// if (x < y) {
+//     x += 10;
+// } else {
+//     x += 1;
+// }
+function hello (name) {
+  console.log('hi', name)   // ✓ 正确
+    console.log('hello', name)   // ✗ 错误
 }
